d672573e0f625ca666706e21264100c5171025bc
jinjoolee1 <jinjoolee126@gmail.com>
2022-05-22 16:30:39 -0500

    index.js in public fixed

--- HTMLroutes/index.js
+++ HTMLroutes/index.js
@@ -3,11 +3,11 @@ const router = require('express').Router();
 
 // html route
 router.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, '../../public/notes.html'));
+    res.sendFile(path.join(__dirname, '.Develop/public/notes.html'));
 });
 
 router.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../../public/index.html'))
+    res.sendFile(path.join(__dirname, '.Develop/public/index.html'))
 })
 
 module.exports = router;
