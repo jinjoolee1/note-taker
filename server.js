960535a4e52904810276f8f4cc0644a8cd408350
jinjoolee1 <jinjoolee126@gmail.com>
2022-05-22 16:42:45 -0500

    server fixed

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ const express = require('express');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
-const apiRoutes = require('./APIroutes');
-const htmlRoutes = require('./HTMLroutes');
+const APIroutes = require('./APIroutes');
+const HTMLroutes = require('./HTMLroutes');
 
 // Middleware
 app.use(express.urlencoded( { extended: true }));
@@ -11,8 +11,8 @@ app.use(express.json());
 app.use(express.static('public'));
 
 // use apiRoutes
-app.use('/api', apiRoutes);
-app.use('/', htmlRoutes);
+app.use('/api', APIroutes);
+app.use('/', HTMLroutes);
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}`);
