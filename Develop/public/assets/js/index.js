8e013cb85cfa4acf758c89b3b23297d01ac003b3
jinjoolee1 <jinjoolee126@gmail.com>
2022-05-22 15:56:13 -0500

    index.js in js updated

--- Develop/public/assets/js/index.js
+++ Develop/public/assets/js/index.js
@@ -181,3 +181,12 @@ if (window.location.pathname === '/notes') {
 }
 
 getAndRenderNotes();
+
+if (window.location.pathname === '/notes') {
+  saveNoteBtn.addEventListener('click', handleNoteSave);
+  newNoteBtn.addEventListener('click', handleNewNoteView);
+  noteTitle.addEventListener('keyup', handleRenderSaveBtn);
+  noteText.addEventListener('keyup', handleRenderSaveBtn);
+}
+
+getAndRenderNotes();
