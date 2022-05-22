c6c5e00b88f7c291bf418988edb560f4423b3396
jinjoolee1 <jinjoolee126@gmail.com>
2022-05-22 16:23:31 -0500

    routes fixed

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ const express = require('express');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
-const apiRoutes = require('./routes/apiRoutes');
-const htmlRoutes = require('./routes/htmlRoutes');
+const apiRoutes = require('./APIroutes');
+const htmlRoutes = require('./HTMLroutes');
 
 // Middleware
 app.use(express.urlencoded( { extended: true }));
